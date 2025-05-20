89bf52e2309391c1846341fea0ae9086c5e464dc
Omanshu Gupta <121303894+Omanshugupta@users.noreply.github.com>
2025-05-20 07:37:39 +0530

    Create 3355_Zero_Array_Transformation_I.py

---
+++ 3355_Zero_Array_Transformation_I.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        count = [0] * (n + 1)
+      
+        for l, r in queries:
+            count[l] += 1
+            if r + 1 < n:
+                count[r + 1] -= 1
+
+        for i in range(1, n):
+            count[i] += count[i - 1]
+
+        for i in range(n):
+            if nums[i] > count[i]:
+                return False
+
+        return True
